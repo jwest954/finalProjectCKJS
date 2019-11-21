aa82d6cd93439ea94bee1c484e006afdd035c6e7
kstellma <stellmachka@gmail.com>
2019-11-21 10:38:03 -0600

    I added a third tab for the plot of us rankings over the years.

--- hw8-app/app.R
+++ hw8-app/app.R
@@ -3,6 +3,8 @@
 library(shiny)
 library(tidyverse)
 library(plotly)
+library(readr)
+library(readxl)
 
 Endowments <- read_csv("Endowments.csv")
 
@@ -11,8 +13,17 @@ tidy_Endowments <- Endowments %>%
                names_to = "year", 
                values_to = "endowment", names_ptypes = list(year=integer())) 
 
+USNews_Rankings <- read_excel("../PCDB_USNews_Rankings.xlsx", 
+                              sheet = "US News Ranks", range = "A2:K43") %>% 
+  rename(College = ...1)
+
+tidy_USNews_Rankings <- USNews_Rankings %>% 
+  pivot_longer(cols = starts_with("20"),
+               names_to = "year", 
+               values_to = "ranking", names_ptypes = list(year=integer()))
+
 ui <- fluidPage(
-  titlePanel("College Endowments"),
+  titlePanel("College Comparison"),
   sliderInput(inputId = "year_range", label = "Year Range", 
               min = 2008, max=2017, value=c(2008,2017), sep = ""),
   selectInput("College1", "College 1:", 
@@ -23,8 +34,8 @@ ui <- fluidPage(
   #submitButton(text = "Create my plot"),
   tabsetPanel(type = "tabs",
               tabPanel("Plot", plotOutput(outputId = "plot")),
-              tabPanel("Plot2", plotlyOutput(outputId = "plot2"))
-              ))
+              tabPanel("Plot2", plotlyOutput(outputId = "plot2")),
+              tabPanel("Plot3",plotlyOutput(outputId = "plot3"))))
   
 
 server <- function(input, output) {
@@ -42,5 +53,13 @@ server <- function(input, output) {
                                          geom_line()+
                                          scale_x_continuous(limits = input$year_range)+
                                          coord_cartesian(ylim = c(0, 3000))))})
-}
+
+  output$plot3 <- renderPlotly({print(ggplotly(tidy_USNews_Rankings %>% 
+                                                 filter(College ==input$College1 | College ==input$College2) %>% 
+                                                 ggplot(aes(x=year, y=ranking, color=College))+
+                                                 geom_line()+
+                                                 scale_x_continuous(limits = input$year_range)+
+                                                 coord_cartesian(ylim = c(0, 50))))})
+  
+  }
 shinyApp(ui = ui, server = server)
