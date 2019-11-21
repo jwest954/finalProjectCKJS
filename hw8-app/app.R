37b17ce0a0d5d479338a1eddcbfa732473152e5d
jwest954 <jwest1@macalester.edu>
2019-11-21 11:07:45 -0600

    minor adjustments and comments, deleted old graph 1

--- hw8-app/app.R
+++ hw8-app/app.R
@@ -8,15 +8,19 @@ library(readxl)
 
 Endowments <- read_csv("Endowments.csv")
 
+#tidy the data by pivoting it
 tidy_Endowments <- Endowments %>% 
   pivot_longer(cols = starts_with("20"),
                names_to = "year", 
                values_to = "endowment", names_ptypes = list(year=integer())) 
 
+#read in the second dataset. Make sure it's still in the right location!
+#The ../ indicates "go out one folder" 
 USNews_Rankings <- read_excel("../PCDB_USNews_Rankings.xlsx", 
                               sheet = "US News Ranks", range = "A2:K43") %>% 
   rename(College = ...1)
 
+#This tidys the second dataset by pivoting it
 tidy_USNews_Rankings <- USNews_Rankings %>% 
   pivot_longer(cols = starts_with("20"),
                names_to = "year", 
@@ -30,36 +34,29 @@ ui <- fluidPage(
               choices=unique(tidy_Endowments$College)),
   selectInput("College2", "College 2:", 
               choices=unique(tidy_Endowments$College)),
-  helpText("This plot will show the changes in the selected colleges' endowment for the selected range."),
+  helpText("These plots show the changes in the selected colleges' rankings for the selected range."),
   #submitButton(text = "Create my plot"),
   tabsetPanel(type = "tabs",
-              tabPanel("Plot", plotOutput(outputId = "plot")),
-              tabPanel("Plot2", plotlyOutput(outputId = "plot2")),
-              tabPanel("Plot3",plotlyOutput(outputId = "plot3"))))
+              tabPanel("Endowments", plotlyOutput(outputId = "plot1")),
+              tabPanel("Ranking", plotlyOutput(outputId = "plot2"))))
   
 
 server <- function(input, output) {
-  output$plot<- renderPlot({
-    tidy_Endowments %>% 
-      filter(College ==input$College1 | College ==input$College2) %>% 
-      ggplot(aes(x=year, y=endowment, color=College))+
-      geom_line()+
-      scale_x_continuous(limits = input$year_range)+
-      coord_cartesian(ylim = c(0, 3000))
-  })
-  output$plot2 <- renderPlotly({print(ggplotly(tidy_Endowments %>% 
+  output$plot1 <- renderPlotly({print(ggplotly(tidy_Endowments %>% 
                                          filter(College ==input$College1 | College ==input$College2) %>% 
                                          ggplot(aes(x=year, y=endowment, color=College))+
                                          geom_line()+
                                          scale_x_continuous(limits = input$year_range)+
                                          coord_cartesian(ylim = c(0, 3000))))})
 
-  output$plot3 <- renderPlotly({print(ggplotly(tidy_USNews_Rankings %>% 
+  output$plot2 <- renderPlotly({print(ggplotly(tidy_USNews_Rankings %>% 
                                                  filter(College ==input$College1 | College ==input$College2) %>% 
                                                  ggplot(aes(x=year, y=ranking, color=College))+
                                                  geom_line()+
                                                  scale_x_continuous(limits = input$year_range)+
-                                                 coord_cartesian(ylim = c(0, 50))))})
+                                                 coord_cartesian(ylim = c(0, 50), xlim = c(2010,2017))))})
   
-  }
+}
+
+
 shinyApp(ui = ui, server = server)
